612cc7679cf055bbef82abc97b5cbcbd3d1f22b7
AkiyamaHaruki <AkiyamaHaruki2024@gmail.com>
2026-04-17 17:29:23 +0900

    Add Material Components dependency for theming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
